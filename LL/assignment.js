97c11948795f776f60b956450fb4d11df87ae5ff
snigdhcinu <snigdhshourya11111998@gmail.com>
2020-11-23 19:57:55 +0530

    adding problem statement for the assignment.js file

---
+++ LL/assignment.js
@@ -0,0 +1,9 @@
+/*
+
+Ques1. Write Algorithm and programme in javascript for Creating a
+ Doubly Circular Linked List which can contain duplicate values. 
+
+Ques 2. Write a programme to insert all values from 
+Doubly Circular Linked List to an array. The output array 
+should contain unique values.
+*/
