d9aa966135db1067b4876c1147a6a7f0dac66cf9
snigdhcinu <snigdhshourya11111998@gmail.com>
2020-11-30 15:19:52 +0530

    adding starting file, and structure for graph using adjacancy list

---
+++ graphs/graphAdjList.js
@@ -0,0 +1,32 @@
+// Graph implementation using adjacency list 
+// reference material :- https://www.freecodecamp.org/news/8-essential-graph-algorithms-in-javascript/
+
+class Vertex{
+  constructor(label){
+    this.label = label;
+    // for unweighted edge, 0 and 1, 
+    // for weighted edge, 0 and weight-of-edge
+    this.visited = 0;
+  }
+}
+
+class Graph{
+  constructor(){
+    this.adjacencyList = {}
+  }
+  addVertex(v){
+
+  }
+  addEdge(src,destn){
+
+  }
+  removeVertex(v){
+
+  }
+  removeEdge(src,destn){
+
+  }
+  showGraph(){
+    
+  }
+}
